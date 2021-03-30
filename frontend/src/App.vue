ba97eda6ff272443c85538a434caef5238bcc171
Heidrun Heidarsdottir <heidrunh17@ru.is>
2021-03-30 00:46:46 +0000

    Created dynamic greeting page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hello :name="name" />
+    <input type="text" v-model="name" placeholder="Enter your name" />
+    <div>
+      <img alt="Vue logo" src="./assets/doggo.jpeg" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hello from "./components/Hello.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Hello,
+  },
+  data() {
+    return {
+      name: "",
+    };
+  },
+};
 </script>
 
 <style>
@@ -24,5 +32,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+input {
+  margin-bottom: 20px;
 }
 </style>
---
+++ frontend/src/components/Hello.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="greeting">
+    <p v-if="name">Helo {{ name }}!</p>
+    <p v-else>Hi am Doggo, who are yu?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  props: {
+    name: String,
+  },
+};
+</script>
